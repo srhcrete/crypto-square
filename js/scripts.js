cbf5cbc9b95fd464901ead289fc7eaf8020c2c3c
David Hale and Sarah Lindmar <david@>
2017-08-17 17:05:49 -0700

    add proper cipher sequence

--- js/scripts.js
+++ js/scripts.js
@@ -6,29 +6,34 @@ $(document).ready(function() {
 });
 
 
-
-function transmogrify(str) {
   var arrs = [];
   var result = '';
+function transmogrify(str) {
+  // var arrs = [];
+  // var result = '';
   var strSquare = Math.ceil(Math.sqrt(str.length));
-  str = str.split('');
+  str = str.replace(/[^]/g, '').split('');
+  // alert(1);
   pushString(str, strSquare);
-  var x = joinCat(str, strSquare);
-  alert(x);
+  joinCat(strSquare);
 }
 
 function pushString(str, num) {
+  // alert(2);
   for (var i = 0; i < num; i++) {
     arrs.push(str.splice(0, num));
   }
+  // alert(3);
 }
 
-function joinCat(arr, num) {
+function joinCat(num) {
+  // alert(4);
   for (var i = 0; i < num; i++) {
     for (var j = 0; j < num; j++) {
-      result += arr[i][j];
+      result += arrs[j][i];
     }
     result += ' ';
   }
-  return result;
+  // alert(5);
+  alert(result);
 }
